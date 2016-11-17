9f0238d0da7692dc8a5c4ffa04ff92cd4222b5e9
Ederson Montagner <ederson@Edersons-MacBook-Pro.local>
2016-11-17 11:09:05 -0200

    Implementing mean based heuristic algorithm

---
+++ HeuristicAlgorithm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ederson Montagner on 11/14/16.
+//
+
+#include "HeuristicAlgorithm.h"
---
+++ HeuristicAlgorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by Ederson Montagner on 11/14/16.
+//
+
+#ifndef PROJETOTG_HEURISTICALGORITHM_H
+#define PROJETOTG_HEURISTICALGORITHM_H
+
+
+class HeuristicAlgorithm {
+
+};
+
+
+#endif //PROJETOTG_HEURISTICALGORITHM_H
